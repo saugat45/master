1482a57b3009a569e488ae5e8432248fe4b404bb
saugat45 <87410762+saugat45@users.noreply.github.com>
2024-06-15 23:34:09 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+    return (<div classname="App">
+        <h1>Hello world, please welcome to me in the React World.</h1>
+        </div>
+    );
+}
+
+export default App;
